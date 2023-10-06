5a5f11a2b87721487dabf035b8e3616918b5bd94
thirumalesh67 <thirumalesh67@gmail.com>
2023-10-06 10:20:38 -0500

    Web App Integrations with models

--- src/web_app/main/views.py
+++ src/web_app/main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 import joblib
+import warnings
+warnings.filterwarnings('ignore')
 
 # Create your views here.
 
 def home_page(request):
     if request.method == 'POST':
-        model = joblib.load('../../models/rforest.pkl')
+        rf_model = joblib.load('../../models/rforest.pkl')
+        knn_model = joblib.load('../../models/knnModel.pkl')
+        gboost_model = joblib.load('../../models/gboostModel.pkl')
+        xgboost_model = joblib.load('../../models/xgboost.pkl')
+        stacking_model = joblib.load('../../models/stackingModel.pkl')
+
 
         grlivarea = float(request.POST.get('GrLivArea'))
         lotarea = float(request.POST.get('LotArea'))
@@ -18,9 +25,15 @@ def home_page(request):
         bsmtfinsf1 = float(request.POST.get('BsmtFinSF1'))
         openporchsf = float(request.POST.get('OpenPorchSF'))
 
-        input_data = [grlivarea, lotarea, totalbsmtsf, bsmtunfsf, garagearea, yearbuilt, lotfrontage, yearremodadd,
+        features = [grlivarea, lotarea, totalbsmtsf, bsmtunfsf, garagearea, yearbuilt, lotfrontage, yearremodadd,
                       bsmtfinsf1, openporchsf]
-        predicted_price = model.predict([input_data])[0]
+        rf_predict = rf_model.predict([features])
+        knn_predict = knn_model.predict([features])
+        gboost_predict = gboost_model.predict([features])
+        xgboost_predict = xgboost_model.predict([features])
+
+        predicted_price = stacking_model.predict([[rf_predict[0], knn_predict[0], gboost_predict[0], xgboost_predict[0]]])[0]
+
         context = {
             'grlivarea': grlivarea,
             'lotarea': lotarea,
